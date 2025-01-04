585b855a37199ddd9646cd26d437b94a8610b496
Kavya-SK-Stack <kavyask548@gmail.com>
2025-01-04 13:39:29 +0530

    'database'

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
@@ -6,6 +8,16 @@ app.get('/', (request, response) => {
     response.json({ message: 'Hello World' });
 });
 
-app.listen(3001, () => {
-    console.log('Server is running @http://localhost:3001');
+app.listen(3003, () => {
+    console.log(`Server is running @http://localhost:3003`);
 });
+
+mongoose.connect(process.env.MONGO)
+
+    .then(() => {
+        console.log('Connected to MongoDB..');
+    })
+     
+    .catch((error) => {
+        console.log('Error connecting to MongoDb', error);
+    })
