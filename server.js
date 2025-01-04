039af58d59142fa083e1656cce81aa878c6f1f63
Kavya-SK-Stack <kavyask548@gmail.com>
2025-01-04 14:22:18 +0530

    'splitting'

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-const express = require('express');
+
 const mongoose = require('mongoose');
 const { MONGO, PORT } = require('./utils/config');
+const app = require('./app');
 
-const app = express();
+mongoose.connect(MONGO)
 
-app.get('/', (request, response) => {
-    response.json({ message: 'Hello World' });
-});
+  .then(() => {
+    console.log("Connected to MongoDB..");
 
-app.listen(PORT, () => {
-    console.log(`Server is running @http://localhost:3003`);
-});
-
-mongoose.connect( MONGO )
-
-    .then(() => {
-        console.log('Connected to MongoDB..');
-    })
-     
-    .catch((error) => {
-        console.log('Error connecting to MongoDb', error);
-    })
+    app.listen(PORT, () => {
+      console.log(`Server is running @http://localhost:3003`);
+    });
+  })
+    
+  .catch((error) => {
+    console.log("Error connecting to MongoDb", error);
+  })
